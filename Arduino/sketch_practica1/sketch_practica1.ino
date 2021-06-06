ff742736bebab07a62497b562287e64b67de8d54
César Alejandro Sosa Enríquez <60756401+Alejandro5852@users.noreply.github.com>
2021-06-05 21:42:37 -0600

    Cambio de modos, implementacion 2 botones para velocidad

--- Arduino/sketch_practica1/sketch_practica1.ino
+++ Arduino/sketch_practica1/sketch_practica1.ino
@@ -8,7 +8,8 @@
 
 //DEFINICION DE PINES
 const int dirPin = 5;
-const int speedPin = 6;
+const int LowSpeedPin = 6;
+const int HighSpeedPin = 53;
 const int textMode = 7;
 const int startPin = 8;
 //BOTONES PARA JUEGO
@@ -17,67 +18,75 @@ const int rightPin = 10;
 const int upPin = 12;
 const int downPin = 13;
 
-//MATRIZ 
+//MATRIZ
 MD_Parola displayMatrix = MD_Parola(HARDWARE_TYPE, DIN, CLK, LOAD, MAX_DEVICES);
 //MENSAJE A MOSTRAR
 const String message = "*TP1 - GRUPO 10 - SECCION A*";
 
 //VELOCIDAD DEL TEXTO
-int textSpeed; 
-//TIEMPO DE ESPERA ENTRE LETRAS INDIVIDUALES   
+int textSpeed;
+//TIEMPO DE ESPERA ENTRE LETRAS INDIVIDUALES
 int waitTime;
-//BANDERA DE CAMBIO DE TEXTO    
+//BANDERA DE CAMBIO DE TEXTO
 bool changeFlag;
 
 int timer = 38;
 int timer2 = 2;
 
 
+
+//Contadores de tiempo
+long CurrentTime;
+long Delimiter;
+
+//modo
+bool onGame;
+
 //TEXTO CON MOVIMIENTO
-void mainText(){
-  if(changeFlag){
+void mainText() {
+  if (changeFlag) {
     displayMatrix.displayText("*TP1 - GRUPO 10 - SECCION A*", PA_LEFT, 100, 0, PA_SCROLL_RIGHT, PA_SCROLL_RIGHT);
     changeFlag = false;
   }
   //Modo de izquierda a derecha
-  if(digitalRead(dirPin)==HIGH){
-    displayMatrix.setTextEffect(PA_SCROLL_LEFT, PA_SCROLL_LEFT); 
-    displayMatrix.setSpeed(map(textSpeed, 1023, 0, 400,15));
+  if (digitalRead(dirPin) == HIGH) {
+    displayMatrix.setTextEffect(PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+    displayMatrix.setSpeed(map(textSpeed, 1023, 0, 400, 15));
   }
   //Modo derecha a izquierda
-  else{
+  else {
     displayMatrix.setTextEffect(PA_SCROLL_RIGHT, PA_SCROLL_RIGHT);
-    displayMatrix.setSpeed(map(textSpeed, 1023, 0, 400,15));
+    displayMatrix.setSpeed(map(textSpeed, 1023, 0, 400, 15));
   }
   //Se reinicia animacion
-  if(displayMatrix.displayAnimate()){
+  if (displayMatrix.displayAnimate()) {
     displayMatrix.displayReset();
   }
 }
 //LETRAS INDIVIDUALES
-void individualLetter(){
-  int i=0;
-  int j=0;
-  int auxI=0;
-  int auxJ=message.length()-1;
+void individualLetter() {
+  int i = 0;
+  int j = 0;
+  int auxI = 0;
+  int auxJ = message.length() - 1;
   changeFlag = true;
   displayMatrix.setTextAlignment(PA_CENTER);
-  
-  while(true){
+
+  while (true) {
     //Modo de izquierda a derecha
-    if(digitalRead(dirPin)==HIGH){
-      for(i=auxI; i<message.length()-1;i++){
-        auxI=0;
-        displayMatrix.print(String(message[i])+" "+String(message[i+1]));
+    if (digitalRead(dirPin) == HIGH) {
+      for (i = auxI; i < message.length() - 1; i++) {
+        auxI = 0;
+        displayMatrix.print(String(message[i]) + " " + String(message[i + 1]));
         delay(500);
         displayMatrix.displayClear();
         delay(waitTime);
-        if(digitalRead(textMode) == LOW){
+        if (digitalRead(textMode) == LOW) {
           displayMatrix.displayClear();
           return;
         }
-        if(digitalRead(dirPin)==LOW){
-          auxJ=i+1;
+        if (digitalRead(dirPin) == LOW) {
+          auxJ = i + 1;
           break;
         }
         //Se verifica si hay cambio de velocidad
@@ -85,134 +94,141 @@ void individualLetter(){
       }
     }
     //Modo derecha a izquierda
-    else{
-      for(j=auxJ;j>0;j--){
-        auxJ=message.length()-1;
-        displayMatrix.print(String(message[j-1])+" "+String(message[j]));
+    else {
+      for (j = auxJ; j > 0; j--) {
+        auxJ = message.length() - 1;
+        displayMatrix.print(String(message[j - 1]) + " " + String(message[j]));
         delay(500);
         displayMatrix.displayClear();
         delay(waitTime);
-        if(digitalRead(textMode) == LOW){
+        if (digitalRead(textMode) == LOW) {
           displayMatrix.displayClear();
           return;
         }
-        if(digitalRead(dirPin)==HIGH){
-          auxI=j-1;
+        if (digitalRead(dirPin) == HIGH) {
+          auxI = j - 1;
           break;
         }
         //Se verifica si hay cambio de velocidad
         setupSpeed();
       }
     }
-    
+
   }
-  
+
 
 }
 
-//LETRAS INDIVIDUALES GAME OVER 
-void individualLetterG(String wordDisplay, int timeToWait){
+//LETRAS INDIVIDUALES GAME OVER
+void individualLetterG(String wordDisplay, int timeToWait) {
   //changeFlag = 1;
   displayMatrix.setTextAlignment(PA_CENTER);
   int longText = wordDisplay.length();
-  for(int i=0; i<longText;i++){
-    if(i== longText-2){
+  for (int i = 0; i < longText; i++) {
+    if (i == longText - 2) {
       displayMatrix.print(score);
       i++;
-    }else{
+    } else {
       char letter = wordDisplay[i];
-      displayMatrix.print(String(wordDisplay[i])+" "+String(wordDisplay[i]));
+      displayMatrix.print(String(wordDisplay[i]) + " " + String(wordDisplay[i]));
     }
     timer--;
     delay(700);
-    if(timer <=0){
+    if (timer <= 0) {
       displayMatrix.displayClear();
       timer = 38;
       break;
     }
   }
 }
-void individualLetterG2(){
-  for(int i=0; i<timer2;i++){
-    if(timer <=0){
+void individualLetterG2() {
+  for (int i = 0; i < timer2; i++) {
+    if (timer <= 0) {
       displayMatrix.displayClear();
       timer = 2;
       break;
     }
     timer2--;
     displayMatrix.setTextEffect(PA_SCROLL_LEFT, PA_SCROLL_LEFT);
-    displayMatrix.print("GAME OVER SCORE"+String(score));
+    displayMatrix.print("GAME OVER SCORE" + String(score));
     delay(2000);
   }
 }
 //CONFIGURA LA VELOCIDAD
-void setupSpeed(){
-  if(digitalRead(speedPin)== LOW){
+void setupSpeed() {
+  if (digitalRead(LowSpeedPin) == LOW) {
     textSpeed = 150;
     waitTime = 750;
   }
-  else{
+  if (digitalRead(HighSpeedPin) == LOW) {
     textSpeed = 50;
     waitTime = 250;
   }
 }
 
-void moveText(){
-  if(digitalRead(textMode)== HIGH){
+void moveText() {
+  if (digitalRead(textMode) == HIGH) {
     individualLetter();
   }
-  else if(digitalRead(textMode)== LOW){
+  else if (digitalRead(textMode) == LOW) {
     mainText();
   }
 }
 
-void gameMode(){
-  unsigned long currentTime = millis(); 
-  if(!gameover){
+void gameMode() {
+  if (digitalRead(startPin) == HIGH) {
+    changeToTextMode();
+  } else {
+    Delimiter = millis();
+    CurrentTime = millis();
+  }
+  unsigned long currentTime = millis();
+  if (!gameover) {
     draw();
     elapsedTime += currentTime - previousTime;
-    if(elapsedTime > 500){
-       move();
-       eat();
-       checkGameover();
-       elapsedTime = 0;
-       readInput = true;
+    if (elapsedTime > 500) {
+      move();
+      eat();
+      checkGameover();
+      elapsedTime = 0;
+      readInput = true;
     }
-    if(readInput){
-      if(digitalRead(rightPin) && !lastInput) {  
-        direction = (direction + 1) % 4; 
-        readInput = false; 
+    if (readInput) {
+      if (digitalRead(rightPin) && !lastInput) {
+        direction = (direction + 1) % 4;
+        readInput = false;
       }
-      if(digitalRead(leftPin) && !lastInput) { 
-        direction = (4 + direction-1) % 4; 
-        readInput = false; 
+      if (digitalRead(leftPin) && !lastInput) {
+        direction = (4 + direction - 1) % 4;
+        readInput = false;
       }
     }
-    lastInput = digitalRead(rightPin) || digitalRead(leftPin);     
+    lastInput = digitalRead(rightPin) || digitalRead(leftPin);
   }
-  else{
-    if(gameover2 != 1){
-      String gameOverText = "GAME OVER SCORE: "+String(score);
-      individualLetterG(gameOverText,waitTime);
+  else {
+    if (gameover2 != 1) {
+      String gameOverText = "GAME OVER SCORE: " + String(score);
+      individualLetterG(gameOverText, waitTime);
       gameover2 = 1;
     }
-    if(currentTime % 800 > 400){
+    if (currentTime % 800 > 400) {
       draw();
       gameover -= currentTime - previousTime;
-      if(gameover <= 0){
+      if (gameover <= 0) {
         reset();
-      } 
-    }  
+      }
+    }
   }
-    previousTime = currentTime;
+  previousTime = currentTime;
 }
 
 //*************************************************************************************************************************************************
 void setup() {
-  
+
   Serial.begin(9600);
   //PINES
-  pinMode(speedPin, INPUT);
+  pinMode(LowSpeedPin, INPUT);
+  pinMode(HighSpeedPin, INPUT);
   pinMode(dirPin, INPUT);
   pinMode(textMode, INPUT);
   pinMode(startPin, INPUT);
@@ -220,20 +236,61 @@ void setup() {
   pinMode(rightPin, INPUT);
   pinMode(upPin, INPUT);
   pinMode(downPin, INPUT);
-  
+
   changeFlag = true;
   displayMatrix.begin();
   displayMatrix.setIntensity(5);
   displayMatrix.displayClear();
 
+
+  textSpeed = 150;
+  waitTime = 750;
+
+
+  CurrentTime = 0;
+  Delimiter = 0;
+
+  onGame = false;
+
 }
 
-void loop(){
-  setupSpeed();
-  if(digitalRead(startPin)== HIGH){
-    gameMode();
+void changeToGameMode() {
+  if (CurrentTime - Delimiter >= 3000) {
+    Delimiter = millis();
+    CurrentTime = millis();
+    onGame = true;
+  } else {
+    CurrentTime = millis();
   }
-  else{
+}
+
+
+void changeToTextMode() {
+  if (CurrentTime - Delimiter >= 3000) {
+    Delimiter = millis();
+    CurrentTime = millis();
+    onGame = false;
+  } else {
+    CurrentTime = millis();
+  }
+}
+
+
+void loop() {
+  setupSpeed();
+  if (digitalRead(startPin) == HIGH) {
+    if (onGame) {
+      changeToTextMode();
+    } else {
+      changeToGameMode();
+    }
+  } else {
+    Delimiter = millis();
+    CurrentTime = millis();
+  }
+  if (onGame) {
+    gameMode();
+  } else {
     moveText();
   }
 }
